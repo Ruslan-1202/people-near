be52cc71f730b82e8f64e234f55e7d369cbb83a4
Ruslan Abushaev <rus021281@gmail.com>
2026-01-20 11:49:19 +0300

    Persons service unit tests

--- src/main/resources/db/changelog/2026/2026_01_16_person_new_fields.sql
+++ src/main/resources/db/changelog/2026/2026_01_16_person_new_fields.sql
@@ -3,10 +3,10 @@ ALTER TABLE person
 ALTER TABLE person
     ALTER COLUMN name DROP NOT NULL;
 ALTER TABLE person
-    ADD COLUMN last_name TEXT NULL;
+    ADD COLUMN IF NOT EXISTS last_name TEXT NULL;
 ALTER TABLE person
-    ADD COLUMN middle_name TEXT NULL;
+    ADD COLUMN IF NOT EXISTS middle_name TEXT NULL;
 ALTER TABLE person
-    ADD COLUMN nick_name TEXT NULL;
+    ADD COLUMN IF NOT EXISTS nick_name TEXT NULL;
 ALTER TABLE person
     ADD CONSTRAINT person_c1_names CHECK ( name IS NOT NULL OR last_name IS NOT NULL OR nick_name IS NOT NULL );
---
+++ src/test/java/ru/practicum/peoplenear/controller/PersonControllerTest.java
@@ -0,0 +1,118 @@
+package ru.practicum.peoplenear.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.peoplenear.model.Person;
+import ru.practicum.peoplenear.repository.PersonRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class PersonControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @BeforeEach
+    void setUp() {
+        personRepository.deleteAll();
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Получить все записи")
+    void shouldFindAllPersons() {
+        createTestPersons();
+        mockMvc.perform(get("/person"))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$", hasSize(2))
+                );
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Создать запись")
+    void shouldCreatePerson() {
+        mockMvc.perform(post("/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                   "name": "Test Create 1",
+                                   "birthDate": "2004-05-23"
+                                }
+                                """)
+                )
+                .andDo(print())
+                .andExpectAll(
+                        jsonPath("$.id", notNullValue()),
+                        jsonPath("$.name", is("Test Create 1"))
+                );
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Получить запись")
+    void shouldGetByIdPerson() {
+        long id = createTestPersons().get(0);
+        mockMvc.perform(get("/person/" + id)
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.id", is((int) id))
+                );
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Удалить запись")
+    void shouldDeletePerson() {
+        var id = createTestPersons().get(0);
+        mockMvc.perform(delete("/person/" + id)
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk()
+                );
+        assertEquals(1, personRepository.count(), "Неверное количество записей после удаления");
+    }
+
+    private List<Long> createTestPersons() {
+        List<Long> personIds = new ArrayList<>();
+        Person person = Person.builder()
+                .name("Test 1")
+                .birthDate(LocalDate.of(2020, 1, 2))
+                .build();
+        person = personRepository.save(person);
+        personIds.add(person.getId());
+        person = Person.builder()
+                .name("Test 2")
+                .birthDate(LocalDate.of(2021, 11, 22))
+                .build();
+        person = personRepository.save(person);
+        personIds.add(person.getId());
+        return personIds;
+    }
+}
